1b0f3f871d6c26e47481fe84223dd68ec60bf0e0
Deasoso <932152571@qq.com>
2018-07-29 18:09:05 +0800

    add eos counter
    it's hard.......

--- Contracts/EOS/counter/counter.cpp
+++ Contracts/EOS/counter/counter.cpp
@@ -9,25 +9,15 @@ class counter_contract : public eosio::contract {
     }
     using eosio::contract::contract;
     void add(account_name receiver) {
-        todos.emplace(author, [&](auto& new_todo) {
-            new_todo.id = id;
-            new_todo.description = description;
-            new_todo.completed = 0;
-        });
         amount += 1;
         eosio::print("Now amount is %d", amount);
     }
+    void sub(account_name receiver){
+        amount -= 1;
+        eosio::print("Now amount is %d", amount);
+    }
   private:
-  // @abi table todos i64
-    struct todo {
-      uint64_t id;
-
-      uint64_t primary_key() const { return id; }
-      EOSLIB_SERIALIZE(todo, (id))
-    };
-
-    typedef eosio::multi_index<N(todos), todo> todo_table;
-    todo_table todos;
+    uint64_t amount;
 };
 
 EOSIO_ABI( counter_contract, (add)(sub) )
