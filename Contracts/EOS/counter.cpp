e158fd5ca4751e7ae65172d91ba782655332a7dd
MinakoKojima <minakokojima@MacBook-Pro.local>
2018-07-29 18:29:31 +0800

    go

--- Contracts/EOS/counter.cpp
+++ Contracts/EOS/counter.cpp
@@ -4,26 +4,27 @@ using namespace eosio;
 
 class counter_contract : public eosio::contract {
   public:
-    counter_contract(account_name self) :eosio::contract(self){
-        amount = 0;
+    counter_contract(account_name self):eosio::contract(self),todos(_slef, _self) {
+        todos.emplace(self, [&](auto& new_todo)) {
+            new_todo.id = 0;
+            new_todo.counter = 0;
+        }
     }
     using eosio::contract::contract;
-    void add(account_name receiver) {
-        todos.emplace(author, [&](auto& new_todo) {
-            new_todo.id = id;
-            new_todo.description = description;
-            new_todo.completed = 0;
+    void add(account_name author) {
+        auto itr = todos.find(0);
+        todos.modify(itr, author, [&](auto& new_todo) {
+            new_todo.counter += 1;
         });
-        amount += 1;
-        eosio::print("Now amount is %d", amount);
+        eosio::print("Now counter is %d", counter);
     }
   private:
   // @abi table todos i64
     struct todo {
-      uint64_t id;
-
-      uint64_t primary_key() const { return id; }
-      EOSLIB_SERIALIZE(todo, (id))
+        uint64_t id;
+        uint64_t counter;
+        uint64_t primary_key() const { return id; }
+        EOSLIB_SERIALIZE(todo, (id)(counter))
     };
 
     typedef eosio::multi_index<N(todos), todo> todo_table;
