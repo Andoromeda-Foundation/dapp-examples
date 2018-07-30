638f05aac6a5adbe9fef53465855ee798049c6d6
gloria12345678 <quyi28266687@gmail.com>
2018-07-30 22:49:30 +0800

    web3

---
+++ src/api/EthWeb.js
@@ -0,0 +1,7 @@
+import web3 from 'web3';
+if (typeof web3 !== 'undefined') {
+    web3 = new Web3(web3.currentProvider);
+  } else {
+    // set the provider you want from Web3.providers
+    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  }
