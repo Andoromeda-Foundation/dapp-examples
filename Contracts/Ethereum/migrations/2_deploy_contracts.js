571d4e9ba5b571964d931dfd538bb07014abbd60
lychees <lychees67@gmail.com>
2018-08-07 17:34:14 +0800

    go

--- Contracts/Ethereum/migrations/2_deploy_contracts.js
+++ Contracts/Ethereum/migrations/2_deploy_contracts.js
@@ -1,7 +1,12 @@
 var Counter = artifacts.require("./Counter.sol");
 var PowhToken = artifacts.require("./PowhToken.sol");
 
+var TradeableToken = artifacts.require("./PonziToken/TradeableToken.sol");
+var ProfitableToken = artifacts.require("./PonziToken/ProfitableToken.sol");
+
 module.exports = function(deployer) {
     deployer.deploy(Counter);
-    deployer.deploy(PowhToken);    
+    deployer.deploy(PowhToken);
+    deployer.deploy(TradeableToken);
+    deployer.deploy(ProfitableToken);    
 };
