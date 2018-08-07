bee827cb4ade218636f9609f43e5b75b48f3b7ee
lychees <lychees67@gmail.com>
2018-08-07 14:40:17 +0800

    Make it compilable.

---
+++ Contracts/Ethereum/contracts/PonziCoin/ReferableToken.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.4.24;
+
+import "./TradeableToken.sol";
+
+contract ReferableToken is TradeableToken {
+
+}
--- Contracts/Ethereum/contracts/PonziToken.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.24;
-
-import "TradeableToken.sol";
-
-contract ShareableToken is TradeableToken {
-
-}
