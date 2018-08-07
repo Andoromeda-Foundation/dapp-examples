53a1a7fb622e76e3ee1e839a5c1f012164349581
lychees <lychees67@gmail.com>
2018-08-07 14:55:22 +0800

    try to use a new etheumToToken function.

--- Contracts/Ethereum/contracts/PowhToken.sol
+++ Contracts/Ethereum/contracts/PowhToken.sol
@@ -608,9 +608,40 @@ contract Hourglass {
             )/(tokenPriceIncremental_)
         )-(tokenSupply_)
         ;
-  
+        require(_tokensReceived == ethereumToTokens2_(_ethereum));
         return _tokensReceived;
     }
+
+    /**
+     * Calculate Token price based on an amount of incoming ethereum
+     * It's an algorithm, hopefully we gave you the whitepaper with it in scientific notation;
+     * Some conversions occurred to prevent decimal errors or underflows / overflows in solidity code.
+     */
+    function ethereumToTokens2_(uint256 _ethereum)
+        internal
+        view
+        returns(uint256)
+    {
+        uint256 _tokenPriceInitial = tokenPriceInitial_ * 1e18;
+        uint256 _tokensReceived = 
+         (
+            (
+                // underflow attempts BTFO
+                SafeMath.sub(
+                    (sqrt
+                        (
+                            (_tokenPriceInitial**2)
+                            +
+                            (2*(tokenPriceIncremental_ * 1e18)*(_ethereum * 1e18))
+                        )
+                    ), _tokenPriceInitial
+                )
+            )/(tokenPriceIncremental_)
+        )
+        ;
+  
+        return _tokensReceived;
+    }    
     
     /**
      * Calculate token sell value.
