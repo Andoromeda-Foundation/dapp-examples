dfebf78cf64ca184589f22770d3330583e3ca1c5
necokeine <necokeine@gmail.com>
2018-08-24 01:47:43 +0800

    Add results.

--- Contracts/EOS/happyeosslot2.0/slot_machine.cpp
+++ Contracts/EOS/happyeosslot2.0/slot_machine.cpp
@@ -13,11 +13,10 @@ class slot_machine : public contract {
     slot_machine(account_name self) : contract(self),
                                       global(_self, _self),
                                       offers(_self, _self),
-                                      last_result(_self, _self) {}
+                                      results(_self, _self) {}
 
     void init(account_name self, const checksum256 &hash) {
         eosio_assert(self == _self, "only contract itself.");
-
         auto g = global.find(0);
         if (g == global.end()) {
             global.emplace(_self, [&](auto &g) {
@@ -48,6 +47,17 @@ class slot_machine : public contract {
             offer.bet = eos.amount;
             offer.seed = seed;
         });
+        auto p = results.find(itr->owner);
+        if (p == results.end()) {
+            p = results.emplace(_self, [&](auto& result) {
+              result.account = itr->owner;
+              result.roll_result = 0;
+            });
+        } else {
+            results.modify(p, 0, [&](auto& result) {
+              results.roll_result = 0;
+            });
+        }
     }
 
     void reveal(const account_name host, const checksum256 &seed, const checksum256 &hash) {
@@ -77,17 +87,6 @@ class slot_machine : public contract {
     typedef eosio::multi_index<N(global), global> global_index;
     global_index global;
 
-    // @abi table player i64
-    struct player {
-        account_name account;
-        uint64_t last_roll;
-
-        uint64_t primary_key() const { return account; }
-        EOSLIB_SERIALIZE(player, (account)(credits))
-    };
-    typedef eosio::multi_index<N(player), player> player_index;
-    player_index players;
-
     // @abi table offer i64
     struct offer {
         uint64_t id;
@@ -101,17 +100,28 @@ class slot_machine : public contract {
     typedef eosio::multi_index<N(offer), offer> offer_index;
     offer_index offers;
 
+    // @abi table result i64
+    struct result {
+        account_name owner;
+        uint64_t roll_number;
+
+        uint64_t primary_key() const { return account; }
+        EOSLIB_SERIALIZE(result, (owner)(roll_number))
+    };
+    typedef eosio::multi_index<N(result), result> result_index;
+    result_index results;
+
     const int p[8] = {   25,   50,  120, 1000, 4000, 20000, 50000, 99999};
     const int b[8] = {10000, 5000, 2000, 1000,  500,   200,    10,     1};
 
-    uint64_t get_bonus(uint64_t seed, uint64_t amount) {
+    uint64_t get_bonus(uint64_t seed) {
         seed %= 100000;
         int i = 0;
         while (seed >= p[i]) {
             seed -= p[i];
             ++i;
         }
-        return b[i] * amount / 100;
+        return b[i];
     }
 
     uint64_t merge_seed(const checksum256 &s1, const checksum256 &s2) {
@@ -123,21 +133,31 @@ class slot_machine : public contract {
     }
 
     void deal_with(eosio::multi_index<N(offer), offer>::const_iterator itr, const checksum256 &seed) {
-        uint64_t bonus = get_bonus(merge_seed(seed, itr->seed), itr->bet);
+        uint64_t bonus_rate = get_bonus(merge_seed(seed, itr->seed));
+        uint64_t bonus = bonus * itr->bet / 100;
         action(
             permission_level{_self, N(active)},
             N(eosio.token), N(transfer),
             make_tuple(_self, itr->owner, asset(bonus, EOS_SYMBOL),
                        std::string("Happy eos slot bonus. happyeosslot.com")))
             .send();
+        auto p = results.find(itr->owner);
+        if (p == results.end()) {
+            p = results.emplace(_self, [&](auto& result) {
+              result.account = itr->owner;
+            });
+        }
+        results.modify(p, 0, [&](auto& result) {
+          results.roll_result = bonus_rate;
+        });
         offers.erase(itr);
     }
 
     checksum256 parse_memo(const std::string &memo) {
-        checksum256 result;
-        memset(&result, 0, sizeof(checksum256));
+        checksum256 checksum;
+        memset(&checksum, 0, sizeof(checksum256));
         for (int i = 0; i < memo.length(); i++) {
-            result.hash[i & 31] ^= memo[i];
+            checksum.hash[i & 31] ^= memo[i];
         }
         return result;
     }
