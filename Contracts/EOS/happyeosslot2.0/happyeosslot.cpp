660fdc4d2a9d1f03bafdcf1cb5e83c965cd18db4
lychees <lychees67@gmail.com>
2018-08-24 13:52:45 +0800

    make it compilable

--- Contracts/EOS/happyeosslot2.0/happyeosslot.cpp
+++ Contracts/EOS/happyeosslot2.0/happyeosslot.cpp
@@ -1,133 +1,67 @@
-#include <eosiolib/eosio.hpp>
-#include <eosiolib/asset.hpp>
-#include <eosiolib/contract.hpp>
 #include <eosiolib/crypto.h>
 
-using namespace eosio;
+#include "happyeosslot.hpp"
 
-#define EOS_SYMBOL S(4, EOS)
-#define TOKEN_CONTRACT N(eosio.token)
-
-class slot_machine : public contract {
-  public:
-    slot_machine(account_name self) : contract(self),
-                                      global(_self, _self),
-                                      offers(_self, _self),
-                                      results(_self, _self) {}
-
-    void init(account_name self, const checksum256 &hash) {
-        eosio_assert(self == _self, "only contract itself.");
-        auto g = global.find(0);
-        if (g == global.end()) {
-            global.emplace(_self, [&](auto &g) {
-                g.id = 0;
-                g.hash = hash;
-            });
-        } else {
-            global.modify(g, 0, [&](auto &g) {
-                g.hash = hash;
-            });
-        }
-    }
-
-    void transfer(account_name from, account_name to, asset eos, std::string memo) {
-        if (to != _self) {
-            return;
-        }
-        require_auth(from);
-        eosio_assert(eos.is_valid(), "Invalid token transfer");
-        eosio_assert(eos.symbol == EOS_SYMBOL, "only core token allowed");
-        eosio_assert(eos.amount > 0, "must bet a positive amount");
-
-        const checksum256 seed = parse_memo(memo);
-
-        offers.emplace(_self, [&](auto &offer) {
-            offer.id = offers.available_primary_key();
-            offer.owner = from;
-            offer.bet = eos.amount;
-            offer.seed = seed;
+void happyeosslot::init(account_name self, const checksum256 &hash) {
+    eosio_assert(self == _self, "only contract itself.");
+    auto g = global.find(0);
+    if (g == global.end()) {
+        global.emplace(_self, [&](auto &g) {
+            g.id = 0;
+            g.hash = hash;
         });
-        auto p = results.find(itr->owner);
-        if (p == results.end()) {
-            p = results.emplace(_self, [&](auto& result) {
-              result.account = itr->owner;
-              result.roll_result = 0;
-            });
-        } else {
-            results.modify(p, 0, [&](auto& result) {
-              results.roll_result = 0;
-            });
-        }
-    }
-
-    void reveal(const account_name host, const checksum256 &seed, const checksum256 &hash) {
-        require_auth(host);
-        eosio_assert(host == _self, "Only happyeosslot can reveal the answer.");
-        assert_sha256((char *)&seed, sizeof(seed), (const checksum256 *)&global.begin()->hash);
-        auto n = offers.available_primary_key();
-        for (int i = 0; i < n; ++i) {
-            auto itr = offers.find(i);
-            deal_with(itr, seed);
-        }
-        auto itr = global.find(0);
-        global.modify(itr, 0, [&](auto &g) {
+    } else {
+        global.modify(g, 0, [&](auto &g) {
             g.hash = hash;
         });
     }
+}
 
-  private:
-  
-    const int p[8] = {   25,   50,  120, 1000, 4000, 20000, 50000, 99999};
-    const int b[8] = {10000, 5000, 2000, 1000,  500,   200,    10,     1};
-
-    uint64_t get_bonus(uint64_t seed) {
-        seed %= 100000;
-        int i = 0;
-        while (seed >= p[i]) {
-            seed -= p[i];
-            ++i;
-        }
-        return b[i];
+void happyeosslot::transfer(account_name from, account_name to, asset eos, std::string memo) {
+    if (to != _self) {
+        return;
     }
+    require_auth(from);
+    eosio_assert(eos.is_valid(), "Invalid token transfer");
+    eosio_assert(eos.symbol == EOS_SYMBOL, "only core token allowed");
+    eosio_assert(eos.amount > 0, "must bet a positive amount");
 
-    uint64_t merge_seed(const checksum256 &s1, const checksum256 &s2) {
-        uint64_t hash = 0, x;
-        for (int i = 0; i < 32; ++i) {
-            hash ^= (s1.hash[i] ^ s2.hash[i]) << ((i & 7) << 3);
-        }
-        return hash;
-    }
+    const checksum256 seed = parse_memo(memo);
 
-    void deal_with(eosio::multi_index<N(offer), offer>::const_iterator itr, const checksum256 &seed) {
-        uint64_t bonus_rate = get_bonus(merge_seed(seed, itr->seed));
-        uint64_t bonus = bonus * itr->bet / 100;
-        action(
-            permission_level{_self, N(active)},
-            N(eosio.token), N(transfer),
-            make_tuple(_self, itr->owner, asset(bonus, EOS_SYMBOL),
-                       std::string("Happy eos slot bonus. happyeosslot.com")))
-            .send();
-        auto p = results.find(itr->owner);
-        if (p == results.end()) {
-            p = results.emplace(_self, [&](auto& result) {
-              result.account = itr->owner;
-            });
-        }
-        results.modify(p, 0, [&](auto& result) {
-          results.roll_result = bonus_rate;
+    offers.emplace(_self, [&](auto &offer) {
+        offer.id = offers.available_primary_key();
+        offer.owner = from;
+        offer.bet = eos.amount;
+        offer.seed = seed;
+    });
+    /*
+    auto p = results.find(itr->owner);
+    if (p == results.end()) {
+        p = results.emplace(_self, [&](auto& result) {
+            result.account = itr->owner;
+            result.roll_result = 0;
         });
-        offers.erase(itr);
-    }
+    } else {
+        results.modify(p, 0, [&](auto& result) {
+            results.roll_result = 0;
+        });
+    }*/
+}
 
-    checksum256 parse_memo(const std::string &memo) {
-        checksum256 checksum;
-        memset(&checksum, 0, sizeof(checksum256));
-        for (int i = 0; i < memo.length(); i++) {
-            checksum.hash[i & 31] ^= memo[i];
-        }
-        return result;
+void happyeosslot::reveal(const account_name host, const checksum256 &seed, const checksum256 &hash) {
+    require_auth(host);
+    eosio_assert(host == _self, "Only happyeosslot can reveal the answer.");
+    assert_sha256((char *)&seed, sizeof(seed), (const checksum256 *)&global.begin()->hash);
+    auto n = offers.available_primary_key();
+    for (int i = 0; i < n; ++i) {
+        auto itr = offers.find(i);
+        deal_with(itr, seed);
     }
-};
+    auto itr = global.find(0);
+    global.modify(itr, 0, [&](auto &g) {
+        g.hash = hash;
+    });
+}
 
 #define EOSIO_ABI_PRO(TYPE, MEMBERS)                                                                                 \
     extern "C"                                                                                                       \
@@ -151,7 +85,7 @@ class slot_machine : public contract {
     }
 
 // generate .wasm and .wast file
-EOSIO_ABI_PRO(slot_machine, (transfer)(init)(reveal))
+EOSIO_ABI_PRO(happyeosslot, (transfer)(init)(reveal))
 
 // generate .abi file
 // EOSIO_ABI(slot_machine, (transfer)(init)(reveal))
--- Contracts/EOS/happyeosslot2.0/happyeosslot.hpp
+++ Contracts/EOS/happyeosslot2.0/happyeosslot.hpp
@@ -3,32 +3,41 @@
  *  @copyright Andoromeda
  */
 #pragma once
-
 #include <eosiolib/asset.hpp>
+#include <eosiolib/contract.hpp>
 #include <eosiolib/eosio.hpp>
 #include <string>
 
+#define EOS_SYMBOL S(4, EOS)
+#define TOKEN_CONTRACT N(eosio.token)
+
 using std::string;
+using namespace eosio;
 
 class happyeosslot : public contract {
 public:
-    happyeosslot(account_name self):contract(self){}
+    happyeosslot(account_name self):contract(self),
+                                    global(_self, _self),
+                                    offers(_self, _self),
+                                    results(_self, _self) {}
     
     void create(account_name issuer, asset maximum_supply);
     
     void issue(account_name to, asset quantity, string memo);
     
     void transfer(account_name from,
-                  account_name to,
-                  asset        quantity,
-                  string       memo);
-    
+                account_name to,
+                asset        quantity,
+                string       memo);
+
+    void init(account_name self, const checksum256& hash);
+    void bet(const account_name account, const uint64_t bet, const checksum256& seed);    
     void sell(uint64_t amount);
     void reveal(const account_name host, const checksum256 &seed, const checksum256 &hash);
     
     inline asset get_supply(symbol_name sym)const;
     inline asset get_balance(account_name owner, symbol_name sym)const;
-    
+
 private:
 
     // @abi table global i64    
@@ -78,7 +87,7 @@ private:
         account_name owner;
         uint64_t roll_number;
 
-        uint64_t primary_key() const { return account; }
+        uint64_t primary_key() const { return owner; }
         EOSLIB_SERIALIZE(result, (owner)(roll_number))
     };
     typedef eosio::multi_index<N(result), result> result_index;
@@ -86,7 +95,11 @@ private:
     
     void sub_balance(account_name owner, asset value);
     void add_balance(account_name owner, asset value, account_name ram_payer);
-    
+    uint64_t merge_seed(const checksum256& s1, const checksum256& s2);    
+    void deal_with(eosio::multi_index< N(offer), offer>::const_iterator itr, const checksum256& seed);
+    uint64_t get_bonus(uint64_t seed);
+    checksum256 parse_memo(const std::string &memo);
+
 public:
     struct transfer_args {
         account_name  from;
@@ -96,16 +109,68 @@ public:
     };
 };
 
-asset token::get_supply(symbol_name sym)const
+asset happyeosslot::get_supply(symbol_name sym)const
 {
     stats statstable(_self, sym);
     const auto& st = statstable.get(sym);
     return st.supply;
 }
 
-asset token::get_balance(account_name owner, symbol_name sym)const
+asset happyeosslot::get_balance(account_name owner, symbol_name sym)const
 {
     accounts accountstable(_self, owner);
     const auto& ac = accountstable.get(sym);
     return ac.balance;
+}
+
+const int p[8] = {   25,   50,  120, 1000, 4000, 20000, 50000, 99999};
+const int b[8] = {10000, 5000, 2000, 1000,  500,   200,    10,     1};
+
+uint64_t happyeosslot::get_bonus(uint64_t seed) {
+    seed %= 100000;
+    int i = 0;
+    while (seed >= p[i]) {
+        seed -= p[i];
+        ++i;
+    }
+    return b[i];
+}
+
+uint64_t happyeosslot::merge_seed(const checksum256 &s1, const checksum256 &s2) {
+    uint64_t hash = 0, x;
+    for (int i = 0; i < 32; ++i) {
+        hash ^= (s1.hash[i] ^ s2.hash[i]) << ((i & 7) << 3);
+    }
+    return hash;
+}
+
+void happyeosslot::deal_with(eosio::multi_index<N(offer), offer>::const_iterator itr, const checksum256 &seed) {
+    uint64_t bonus_rate = get_bonus(merge_seed(seed, itr->seed));
+    uint64_t bonus = bonus * itr->bet / 100;
+    action(
+        permission_level{_self, N(active)},
+        N(eosio.token), N(transfer),
+        make_tuple(_self, itr->owner, asset(bonus, EOS_SYMBOL),
+                    std::string("Happy eos slot bonus. happyeosslot.com")))
+        .send();
+    auto p = results.find(itr->owner);
+    
+    if (p == results.end()) {
+        p = results.emplace(_self, [&](auto& result) {
+            result.owner = itr->owner;
+        });
+    }
+    results.modify(p, 0, [&](auto& result) {
+        result.roll_number = bonus_rate;
+    });
+    offers.erase(itr);
+}
+
+checksum256 happyeosslot::parse_memo(const std::string &memo) { // to bo refind.
+    checksum256 checksum;
+    memset(&checksum, 0, sizeof(checksum256));
+    for (int i = 0; i < memo.length(); i++) {
+        checksum.hash[i & 31] ^= memo[i];
+    }
+    return checksum;
 }
